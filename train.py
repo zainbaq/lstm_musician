c6149ac431946ff4a7f55299ba37158ce2231563
Zain Baquar <zain.baq@gmail.com>
2021-05-28 19:09:45 -0400

    train.py

--- train.py
+++ train.py
@@ -1,22 +1,20 @@
-import numpy as np
-import pandas as pd
-import glob
-import os
 from tqdm import tqdm
-import torchaudio
 import torch
 import torch.nn as nn
-import matplotlib.pyplot as plt
-from torch.utils.data import Dataset, DataLoader, random_split
+import torchaudio
+from torch.utils.data import DataLoader, random_split
 import argparse
-from preprocess import WaveformDataset
-from models import LSTMModel
+from datasets import WaveformDataset
+from models.definitions.lstm_model import LSTMModel
+
+torchaudio.set_audio_backend("soundfile")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_path', nargs="?", default="data/wav/", help='path to .WAV files.')
 parser.add_argument('--sequence_length', nargs="?", default=6000, type=int, help='sequence length to train on.')
 parser.add_argument('--batch_size', nargs="?", default=128, type=int, help='batch size to train on.')
 parser.add_argument('--n_epochs', nargs="?", default=10, type=int, help='number off epochs to train over.')
+parser.add_argument('--save_path', nargs="?", default='models/binaries/checkpoint.pt', type=str, help='save path.')
 
 args = parser.parse_args()
 
@@ -39,10 +37,11 @@ def main():
     trainloader = DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
     validloader = DataLoader(validset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
 
-    model = LSTMModel(2, SEQUENCE_LENGTH, HIDDEN_SIZE, 2, BATCH_SIZE, 2, DEVICE).to(DEVICE)
-
+    model = LSTMModel(SEQUENCE_LENGTH, HIDDEN_SIZE, BATCH_SIZE, 2, DEVICE).to(DEVICE)
     criterion = nn.L1Loss().to(DEVICE)
     optimizer = torch.optim.AdamW(model.parameters(), lr=5e-4)
+
+    print(model)
 
     for epoch in range(N_EPOCHS):
         model.train()
@@ -80,4 +79,10 @@ def main():
             valid_loss += error.item()
         valid_loss = valid_loss / len(validloader)
         
+        model.save_checkpoint(epoch, optimizer, loss.item(), args.save_path)
+
         print(f'{epoch} - train: {train_loss}, valid: {valid_loss}')
+
+if __name__ == '__main__':
+    main()
+
